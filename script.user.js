eaa036d15c9a495db6ea0ab014e299f3eb9d0479
World_Languages <mundofinky@gmail.com>
2016-11-05 00:35:28 -0300

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -147,18 +147,7 @@ b[divtofind-1].innerHTML = a;
 
 
 
-// Cookies functions
-function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
-}
-
+// Cookies function
 function createCookie(name,value,days) {
 	if (days) {
 		var date = new Date();
@@ -167,8 +156,4 @@ function createCookie(name,value,days) {
 	}
 	else var expires = "";
 	document.cookie = name+"="+value+expires+"; path=/";
-}
-
-function eraseCookie(name) {
-	createCookie(name,"",-1);
 }
