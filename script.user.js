6adcc564caf1315d0f4ff22fab6e4639cdc4129b
World_Languages <mundofinky@gmail.com>
2016-11-05 11:24:05 -0300

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -147,7 +147,7 @@ b[divtofind-1].innerHTML = a;
 
 
 
-// Cookies functions
+// Cookies function
 function readCookie(name) {
 	var nameEQ = name + "=";
 	var ca = document.cookie.split(';');
@@ -157,18 +157,4 @@ function readCookie(name) {
 		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
 	}
 	return null;
-}
-
-function createCookie(name,value,days) {
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
-	}
-	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/";
-}
-
-function eraseCookie(name) {
-	createCookie(name,"",-1);
 }
